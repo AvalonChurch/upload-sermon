456c08da0a5a66f46963c48251b07a859c60f044
Richard Mahn <rich.mahn@unfoldingword.org>
2019-02-18 10:38:37 -0700

    Add scripture.php

--- sermons.php
+++ sermons.php
@@ -791,7 +791,7 @@ function redo_all_sermons() {
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    $sql = "SELECT * FROM " . $prefix . "sermon_sermons WHERE audiofile LIKE '%2016-11-27%' ORDER BY sermon_date ASC";
+    $sql = "SELECT * FROM " . $prefix . "sermon_sermons WHERE audiofile LIKE '%201%' ORDER BY sermon_date ASC";
     $result = $conn->query($sql);
     while($row = mysqli_fetch_assoc($result)){
         print_r($row);
