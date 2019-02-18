920ff24aee23fb39981037034daaef24d68902a6
Richard Mahn <rich.mahn@unfoldingword.org>
2019-02-18 08:06:46 -0700

    Add scripture.php

--- scripture.php
+++ scripture.php
@@ -5,8 +5,9 @@ require_once('docx.php');
 try {
     $docText = RD_Text_Extraction::convert_to_text('../sermonspeaker/sermons/2016-11-06_Single-Minded_BCCC.pptx');
     preg_match_all('/【([^】]+)】/', $docText, $matches, PREG_PATTERN_ORDER);
-    $scriptures = implode("\n", array_slice($matches[1], 1));
+    $scriptures = implode("\n", $matches[1]);
     print_r($scriptures);
+    print_r($matches);
 } catch(Exception $e) {
     echo $e->getMessage();
 }
