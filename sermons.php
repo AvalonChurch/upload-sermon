932351fb0518960e663a8214e940b6f265aae553
Richard Mahn <rich.mahn@unfoldingword.org>
2019-02-18 11:55:09 -0700

    Add scripture.php

--- sermons.php
+++ sermons.php
@@ -94,6 +94,7 @@ function cleanUpScripture($scripture) {
     if(! preg_match('/[A-Z]/', $scripture)) {
         $scripture = preg_replace_callback('/【([^0-9abc ,;-]+)(.*)】/', function ($matches) {
             global $chineseToEnglish;
+            echo "HERE\n";
             print_r($matches);
             if($matches && $chineseToEnglish[$matches[1]])
                 return $matches[1].' '.$chineseToEnglish[$matches[1]].' '.trim(matches[2]);
@@ -800,7 +801,7 @@ function redo_all_sermons() {
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    $sql = "SELECT * FROM " . $prefix . "sermon_sermons WHERE sermon_date LIKE '2017-05%' ORDER BY sermon_date ASC";
+    $sql = "SELECT * FROM " . $prefix . "sermon_sermons WHERE sermon_date LIKE '2017-05-27%' ORDER BY sermon_date ASC";
     $result = $conn->query($sql);
     while($row = mysqli_fetch_assoc($result)){
         print_r($row);
